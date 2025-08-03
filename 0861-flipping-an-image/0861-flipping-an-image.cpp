71ec9ee5d800960b707f2d24fb205b506dc50870
Kartik Srivastava <kartiksrivastava9155@gmail.com>
2025-08-03 22:13:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (33.89%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int m = image.size();
+        int n = image[0].size();
+        for (int i = 0; i < m; i++) {
+            int left = 0;
+            int right = image[i].size() - 1;
+            while (left < right) {
+                swap(image[i][left], image[i][right]);
+                left++;
+                right--;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                image[i][j] = image[i][j]^1;
+            }
+        }
+        return image;
+    }
+};
