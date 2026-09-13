74829cda285b0634cb27b1e28a8335a4435214f2
Kartik Srivastava <kartiksrivastava9155@gmail.com>
2026-09-13 10:55:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.56%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int reverse(int x) {
         int rev = 0;
-        while (x != 0) {
+        while (x) {
             int digit = x % 10;
             if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
                 return 0;
